4a41f5d9e7ae56e7c137a1f108affdea86b43b46
talhaimran2004 <muhammadtalha1400@gmail.com>
2022-10-23 12:45:57 +0500

    affan shop responsive

---
+++ Shop Page/category.js
@@ -0,0 +1,6 @@
+let sidebar = document.getElementById('sidebar');
+let filterBtn = document.getElementById('filter-btn');
+
+filterBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('ml-[900px]')
+})
