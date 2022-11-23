56a1ae7d26060aa8afbbdd2956e7ff53a7422cd0
talhaimran2004 <muhammadtalha1400@gmail.com>
2022-11-23 14:08:34 +0500

    main age calc

--- category.js
+++
@@ -1,6 +0,0 @@
-let sidebar = document.getElementById('sidebar');
-let filterBtn = document.getElementById('filter-btn');
-
-filterBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('ml-[900px]')
-})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["*"],
   theme: {
+    extend: {},
   },
   plugins: [],
 }
